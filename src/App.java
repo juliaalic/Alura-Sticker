20aec95f52615bee2ca9c12de5e1a634a42a36a0
juliaalic <juliaalicelima@gmail.com>
2023-04-05 20:32:05 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,35 @@
+import java.io.InputStream;
+import java.net.URL;
+import java.util.List;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        API api = API.IMDB_TOP_SERIES;
+
+        String url = api.getUrl();
+        ExtratorDeConteudo extrator = api.getExtrator();
+
+        var http = new ClienteHttp();
+        String json = http.buscaDados(url);
+
+        // exibir e manipular os dados 
+        List<Conteudo> conteudos = extrator.extraiConteudos(json);
+
+        var geradora = new GeradorDeFigurinhas();
+
+        for (int i = 0; i < 3; i++) {
+
+            Conteudo conteudo = conteudos.get(i);
+
+            InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
+            String nomeArquivo = "saida/" + conteudo.getTitulo() + ".png";
+
+            geradora.cria(inputStream, nomeArquivo);
+
+            System.out.println(conteudo.getTitulo());
+            System.out.println();
+        }
+    }
+}
